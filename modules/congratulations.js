f9fef9d836da2d1ada3f0e4fff204281b73be4c4
Steve Decker <sdecker@testout.com>
2021-01-27 16:37:53 -0700

    fixed broken congratulations gif

--- modules/congratulations.js
+++ modules/congratulations.js
@@ -5,7 +5,7 @@ var congratArray =
 "https://i.imgur.com/s9bPVaK.gif", //Leo
 "https://i.giphy.com/xeXEpUVvAxCV2.gif", //Confetti
 "https://thumbs.gfycat.com/ElasticAnxiousHornet-small.gif", //Wayne
-"https://www.clickz.com/wp-content/uploads/sites/2/2016/05/congratulations-gif.gif", //Shia
+"https://i.gifer.com/6tV.gif", //Seinfeld Dance
 "https://78.media.tumblr.com/a23fb6be88127e8bb6fa36a63786b331/tumblr_nj08kejyUd1smcbm7o1_500.gif", //Magneto
 "https://i.giphy.com/cyyac9sTiN7ji.gif", //Carlton
 "https://media1.tenor.com/images/8770ef5b6a1c4e54ac7d0f19736f927b/tenor.gif", //Picard
